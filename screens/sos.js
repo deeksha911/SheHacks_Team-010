bdc5c2d2e228e370126ba06ce8e08f778fe4cf19
Nabnita <nabantaborkakoti@gmail.com>
2021-03-14 08:42:40 +0530

    Update sos.js

--- screens/sos.js
+++ screens/sos.js
@@ -8,14 +8,53 @@ import { Card,Title,Paragraph } from 'react-native-paper';
 import SafeAreaView from 'react-native-safe-area-view';
 
 function SosScreen({Navigation}) {
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-        <ScrollView>
-        <Text>sos</Text>
-        </ScrollView>
-        </SafeAreaView>
+    const createTwoButtonAlert = () =>
+    Alert.alert(
+      "Alert",
+      "Send Email to Emergency Number.",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ],
+      { cancelable: false }
     );
-        
+
+    return (
+        <View style={styles.container}>
+            <Text>Need Help?Shake Your Phone!</Text>
+            <Button title="Shake" onPress={createTwoButtonAlert} />
+        </View>
+      );
+    }
+    
+const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    justifyContent: "space-around",
+    alignItems: "center"
+    }
+});
+export default SosScreen;
+
+/*
+onShake();{
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+    to: email,
+    from: 'email@example.com',
+    subject: 'Example Email',
+    text: `
+    Dear user, .
+    `,
+    html: `
+    <p>Dear user,</p>    <p>I need your help!.</p>
+    `,
+};
+sgMail.send(msg);
 }
 
 export default SosScreen;
@@ -26,4 +65,4 @@ const styles = StyleSheet.create({
         alignContent:'center',
         justifyContent:'center'
     }
-});
+});*/
